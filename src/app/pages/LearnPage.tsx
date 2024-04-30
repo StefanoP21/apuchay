128bde456aa49708a6df441a4047a0acd8833bb3
Stefano <stefanop21@outlook.es>
2024-04-30 18:20:16 -0500

    update: página LearnPage

--- src/app/pages/LearnPage.tsx
+++ src/app/pages/LearnPage.tsx
@@ -1,3 +1,19 @@
+import { Container, Heading } from '@chakra-ui/react';
+import { LearnList } from '../';
+
 export const LearnPage = () => {
-  return <div>LearnPage</div>;
+  return (
+    <Container maxWidth="8xl" padding={10}>
+      <Heading
+        size="xl"
+        marginBottom={10}
+        color="red.600"
+        textAlign="center"
+        textTransform="uppercase"
+      >
+        ¡Capacítate con nosotros!
+      </Heading>
+      <LearnList courses={[]} />
+    </Container>
+  );
 };
