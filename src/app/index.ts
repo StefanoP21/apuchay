364ec20ad09e08eedacb6255b376917cb056a3d6
Stefano <stefanop21@outlook.es>
2024-04-30 18:20:46 -0500

    update: archivo de barril

--- src/app/index.ts
+++ src/app/index.ts
@@ -4,7 +4,15 @@ export { hero, goal } from './lib/data';
 
 export { HomePage } from './pages/HomePage';
 export { ProjectsPage } from './pages/ProjectsPage';
+export { ProjectByIdPage } from './pages/ProjectByIdPage';
+export { LearnPage } from './pages/LearnPage';
 
-export { ProjectCard } from './components/ProjectCard';
-export { ProjectList } from './components/ProjectList';
 export { ProjectSelect } from './components/ProjectSelect';
+export { ProjectList } from './components/ProjectList';
+export { ProjectCard } from './components/ProjectCard';
+
+export { ProjectByIdCard } from './components/ProjectByIdCard';
+export { ProjectTable } from './components/ProjectTable';
+
+export { LearnList } from './components/LearnList';
+export { LearnCard } from './components/LearnCard';
