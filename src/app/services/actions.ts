e17826ed5b51a243d47c03d7e3a0c0eaf34bd68e
Stefano <stefanop21@outlook.es>
2024-04-30 20:55:07 -0500

    new: acciones para peticiones de datos

---
+++ src/app/services/actions.ts
@@ -0,0 +1,36 @@
+import { api } from '../../api';
+import type { Project } from '../';
+
+export const getProjects = async (): Promise<Project[]> => {
+  try {
+    const { data } = await api.get<{ ok: boolean; projects: Project[] }>(
+      '/api/projects'
+    );
+
+    if (typeof data !== 'object') {
+      throw new Error('Invalid data type');
+    }
+
+    return data.projects;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const getProjectById = async (id: string): Promise<Project> => {
+  try {
+    const { data } = await api.get<{ ok: boolean; project: Project }>(
+      `/api/projects/${id}`
+    );
+
+    if (typeof data !== 'object') {
+      throw new Error('Invalid data type');
+    }
+
+    return data.project;
+  } catch (error) {
+    console.error(error);
+    return {} as Project;
+  }
+};
