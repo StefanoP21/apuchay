66a2905c470f9e2daa580a7aeb282bdbe1f859aa
Stefano <stefanop21@outlook.es>
2024-04-30 20:54:13 -0500

    new: interface de obra

---
+++ src/app/interfaces/project.ts
@@ -0,0 +1,20 @@
+export interface Project {
+  _id: string;
+  infobrasCode: string;
+  imageUrl: string;
+  name: string;
+  entity: string;
+  type: string;
+  status: string;
+  startDate: string;
+  endDate: string;
+  location: string;
+  address: string;
+  amount: string;
+  enterprise: string;
+  ruc: string;
+  contact: string;
+  dni: string;
+  cip: string;
+  __v: number;
+}
