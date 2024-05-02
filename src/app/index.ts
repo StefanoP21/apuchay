f42f3fc81d351d899a0071c9afe794f3cea5fbd6
Stefano <stefanop21@outlook.es>
2024-05-02 18:45:08 -0500

    update: archivo de barril

--- src/app/index.ts
+++ src/app/index.ts
@@ -5,7 +5,8 @@ export { hero, goal } from './lib/data';
 export { HomePage } from './pages/HomePage';
 export { ProjectsPage } from './pages/ProjectsPage';
 export { ProjectByIdPage } from './pages/ProjectByIdPage';
-export { LearnPage } from './pages/LearnPage';
+export { CoursePage } from './pages/CoursePage';
+export { CourseByIdPage } from './pages/CourseByIdPage';
 
 export { HomeCard } from './components/home/HomeCard';
 
@@ -16,13 +17,17 @@ export { ProjectCard } from './components/project/ProjectCard';
 export { ProjectByIdCard } from './components/projectById/ProjectByIdCard';
 export { ProjectTable } from './components/projectById/ProjectTable';
 
-export { LearnList } from './components/learn/LearnList';
-export { LearnCard } from './components/learn/LearnCard';
+export { CourseList } from './components/course/CourseList';
+export { CourseCard } from './components/course/CourseCard';
 
 export type { Project } from './interfaces/project';
+export type { Course } from './interfaces/course';
 
-export * as projectActions from './services/actions';
+export * as projectActions from './services/projectActions';
+export * as courseActions from './services/courseActions';
 
 export { useProjects } from './hooks/useProjects';
 export { useProject } from './hooks/useProject';
 export { usePrefetchProject } from './hooks/usePrefetchProject';
+
+export { useCourses } from './hooks/useCourses';
