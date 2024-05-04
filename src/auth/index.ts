3af4604fa1eb8c7bd34721bdf7fa7c265c69d33f
Stefano <stefanop21@outlook.es>
2024-05-04 17:09:29 -0500

    update: archivo de barril

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -2,3 +2,10 @@ export { SignInPage } from './pages/SignInPage';
 export { SignUpPage } from './pages/SignUpPage';
 
 export { PasswordField } from './components/PasswordField';
+
+export type { User } from './interfaces/user';
+export type { UserForm } from './interfaces/userForm';
+
+export * as authActions from './services/authActions';
+
+export { useSignIn } from './hooks/useSignIn';
