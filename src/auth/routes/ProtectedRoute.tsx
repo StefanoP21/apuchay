db2543ec7144f4e06ff7869def264392ea7cc3e1
Stefano <stefanop21@outlook.es>
2024-05-04 23:45:10 -0500

    update: protección de rutas

--- src/auth/routes/ProtectedRoute.tsx
+++ src/auth/routes/ProtectedRoute.tsx
@@ -2,9 +2,9 @@ import { PropsWithChildren } from 'react';
 import { Navigate } from 'react-router-dom';
 
 export const ProtectedRoute = ({ children }: PropsWithChildren) => {
-  const status = 'authenticated';
+  const token = localStorage.getItem('token') as string;
 
-  if (status !== 'authenticated') {
+  if (!token) {
     return <Navigate to="/auth/signin" />;
   }
 
