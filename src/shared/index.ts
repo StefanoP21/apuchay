e25696c6e488a4e25c17a18d65e8f6ad0aeea23d
Stefano <stefanop21@outlook.es>
2024-05-06 20:43:59 -0500

    update: archivo de barril

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -2,3 +2,4 @@ export { ErrorPage } from './pages/ErrorPage';
 
 export { NavBar } from './components/NavBar';
 export { Footer } from './components/Footer';
+export { Profile } from './components/Profile';
