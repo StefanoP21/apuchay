db091e45378baa931cd9afde3019c508a0d2a3ed
Stefano <stefanop21@outlook.es>
2024-05-07 20:49:01 -0500

    new: hook con useMutation para el checkAuth

--- src/auth/hooks/useCheckAuth.tsx
+++ src/auth/hooks/useCheckAuth.tsx
@@ -1,21 +1,17 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { useNavigate } from 'react-router-dom';
 
 import { authActions } from '..';
 
-export const useSignUp = () => {
+export const useCheckAuth = () => {
   const queryClient = useQueryClient();
-  const navigate = useNavigate();
 
   const mutation = useMutation({
-    mutationFn: authActions.signUp,
+    mutationFn: authActions.checkAuth,
     onSuccess: (user, _variables, _context) => {
       queryClient.setQueryData(
         ['user', { name: user.name, uid: user.uid }],
         user
       );
-
-      navigate('/courses');
     },
     onError: (error, _variables, _context) => {
       queryClient.setQueryData(['user'], error);
