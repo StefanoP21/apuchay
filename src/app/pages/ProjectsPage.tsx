899c0fa77e818154283de04d6d36bf8d8d262f76
Stefano <stefanop21@outlook.es>
2024-05-11 12:36:32 -0500

    update: página de obras

--- src/app/pages/ProjectsPage.tsx
+++ src/app/pages/ProjectsPage.tsx
@@ -1,9 +1,8 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-import { Container, Button, Divider, Heading } from "@chakra-ui/react";
-import { Search2Icon } from "@chakra-ui/icons";
+import { Container, Heading } from '@chakra-ui/react';
 
-import { ProjectList, ProjectSkeleton, useProjects } from "..";
+import { ProjectList, ProjectSkeleton, useProjects } from '..';
 
 export const ProjectsPage = () => {
   const { isLoading, projects } = useProjects();
@@ -12,43 +11,11 @@ export const ProjectsPage = () => {
     window.scrollTo(0, 0);
   }, []);
 
-  const handleSearch = () => {
-    console.log(isLoading);
-  };
-
   return (
     <Container maxWidth="8xl" padding={10}>
       <Heading size="lg" marginBottom={10} color="red.600">
         Obras para la Reconstrucción con Cambios 2023
       </Heading>
-      {/*<SimpleGrid minChildWidth="xs" spacing={10}>
-        <ProjectSelect
-          title="Departamento"
-          items={[{ id: '15', name: 'Lima' }]}
-          onSelect={() => {}}
-        />
-        <ProjectSelect
-          title="Provincia"
-          items={[{ id: '1501', name: 'Yauyos' }]}
-          onSelect={() => {}}
-        />
-        <ProjectSelect
-          title="Distrito"
-          items={[{ id: '150101', name: 'Yauyos' }]}
-          onSelect={() => {}}
-        />
-      </SimpleGrid>*/}
-      <Button
-        leftIcon={<Search2Icon />}
-        colorScheme="red"
-        size="md"
-        variant="solid"
-        marginTop={5}
-        onClick={handleSearch}
-      >
-        Buscar Obras
-      </Button>
-      <Divider marginY={10} />
       {isLoading ? <ProjectSkeleton /> : <ProjectList projects={projects} />}
     </Container>
   );
